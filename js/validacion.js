a42b3e756b3297eec7b58a34a2be2e2b12a7c28c
Javier Almeida <108470987+javieralmeida30@users.noreply.github.com>
2022-08-21 20:47:35 -0300

    elementos

--- js/validacion.js
+++ js/validacion.js
@@ -1,8 +1,16 @@
-
 function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
+    correcto.classList.add("show");
 }
 
 function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+    error.classList.add("show");
 }
+const nombre = document.getElementById("nombre")
+const apellido = document.getElementById("apellido")
+const email = document.getElementById("email")
+const contrasena = document.getElementById("password1")
+const contrasena2 = document.getElementById("password2")
+const aceptarTerminos = document.getElementById("terminos")
+const registro = document.getElementById("registro")
+const error =  document.getElementById("alert-danger")
+const correcto = document.getElementById("alert-success")
