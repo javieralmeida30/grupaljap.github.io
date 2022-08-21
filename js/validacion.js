d082c6029444616573a1141fac2880c7a8a52459
eliggz <106848206+eliggz@users.noreply.github.com>
2022-08-21 20:49:22 -0300

    function todoCorrecto()

--- js/validacion.js
+++ js/validacion.js
@@ -14,3 +14,33 @@ const aceptarTerminos = document.getElementById("terminos")
 const registro = document.getElementById("registro")
 const error =  document.getElementById("alert-danger")
 const correcto = document.getElementById("alert-success")
+function todoCorrecto(event){
+    event.preventDefault();
+    if(nombre.value=== ""){
+        return showAlertError();
+    }
+    if(apellido.value===""){
+    return showAlertError();
+    }
+    if(email.value===""){
+    return showAlertError();
+    }
+    if(contrasena.value.length < 6){
+    return showAlertError();
+    }
+    if(contrasena2.value != contrasena.value){
+        return showAlertError();
+    }
+    if(!aceptarTerminos.checked){
+    return showAlertError();
+    } 
+    if(nombre.value.length>0 &&
+        apellido.value.length>0 &&
+        email.value.length>0 &&
+        contrasena.value.length>=6 &&
+        contrasena2.value.length>=6 &&
+        contrasena.value===contrasena2.value &&
+        aceptarTerminos.checked){
+            return showAlertSuccess();
+        }
+}
